c3ace1a58fe413076651154cf06f07c7e4ef0b3d
Victor <uberman59@gmail.com>
2023-08-24 00:45:30 +0500

    almost final release

--- dist/_nuxt/static/1692810665/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-nuxt2\u002F_nuxt\u002Fstatic\u002F1692810665",layout:"default",error:a,state:{gallery:{gallery:[]}},serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Ftest-nuxt2\u002F",assetsPath:"\u002Ftest-nuxt2\u002F_nuxt\u002F",cdnURL:a}}}}(null));
